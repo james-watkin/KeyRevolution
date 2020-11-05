1c1fc9f363e789251041ebad286d3957efbb74e6
james-watkin <jameswatkins1260@gmail.com>
2020-11-04 20:31:48 -0800

    Easy med hard buttons hiding other buttons

--- javascript/event_listeners.js
+++ javascript/event_listeners.js
@@ -1,8 +1,59 @@
 
 
 const eventListeners = (canvas,game,ctx) => {
+    // clicking difficulties buttons.
+    const BUTTON_NAMES = [
+        "a-button","s-button","d-button","f-button",
+        "j-button","k-button","l-button",";-button",
+        "space-button"]
 
+    const easy = document.getElementById("easy-button");
+    easy.onclick = () => {
+        BUTTON_NAMES.forEach(idName => {
+            let ele = document.getElementById(idName)
+            if(idName === "a-button" || idName === "s-button" || idName === "l-button" || idName === ";-button"){
+                ele.className = idName;
+            }else{
+                ele.className = "disabled-button";
+            }
+        })
+    }
+    
+    const medium = document.getElementById("medium-button");
+    medium.onclick = () => {
+        BUTTON_NAMES.forEach(idName => {
+            let ele = document.getElementById(idName)
+            if(idName === "a-button" || idName === "s-button" || idName === "l-button" || idName === ";-button"
+                || idName === "d-button" || idName === "k-button" ){
+                ele.className = idName;
+            }else{
+                ele.className = "disabled-button";
+            }
+        })
+    }
 
+    const hard = document.getElementById("hard-button");
+    hard.onclick = () => {
+        BUTTON_NAMES.forEach(idName => {
+            let ele = document.getElementById(idName)
+            if(idName === "a-button" || idName === "s-button" || idName === "l-button" || idName === ";-button"
+                || idName === "d-button" || idName === "k-button"
+                || idName === "j-button" || idName === "f-button" ){
+                ele.className = idName;
+            }else{
+                ele.className = "disabled-button";
+            }
+        })
+    }
+    const extreme = document.getElementById("extreme-button");
+    extreme.onclick = () => {
+        BUTTON_NAMES.forEach(idName => {
+            let ele = document.getElementById(idName)
+            ele.className = idName
+        })
+    }
+
+    // Listening for key presses
     document.addEventListener("keydown", (event)=>{
         switch(event.keyCode){
             case 65: // A Key
