d31973cbf57f9fe5016f51639fc2ae0860f410ed
james-watkin <jameswatkins1260@gmail.com>
2020-11-07 15:19:50 -0800

    Start Screen

--- javascript/start_view.js
+++ javascript/start_view.js
@@ -6,8 +6,14 @@ class StartView {
     }
 
     start() {
+        let startButton = document.getElementById("start-button")
+        let startScreen = document.getElementById("start-screen-outer-modal");
 
-        this.game.start(); // Starts the actual game
+        startButton.addEventListener("click", () => {
+            startScreen.style.display = "none";
+            this.game.start()
+        })
+        // this.game.start(); // Starts the actual game
     }
 }
 
